8af4a368525c110cb28f035da24aa593bde7a70a
Wilman Sirit <wsirit@gmail.com>
2021-09-19 15:22:00 -0400

    Refactoring the User Module

---
+++ controllers/User.js
@@ -0,0 +1,88 @@
+'use strict'
+
+
+const errors = require('restify-errors')
+    , bycript = require('bcryptjs')
+    , jwt = require('jsonwebtoken')
+    , User = require('../models/User')
+    , auth = require('../auth')
+    , config = require('../config');
+
+
+module.exports = {
+
+    get: async (req, res, next) => {
+
+        const users = await User.find();
+
+        try {
+
+            res.send(users);
+            res.send(200)
+            next();
+
+        } catch (err) {
+
+            return next(new errors.InternalError(err.message));
+
+        }
+    },
+
+    addNew: async (req, res, next) => {
+
+        // Check for json
+        if (!req.is('application/json')) return next(new errors.InvalidHeaderError("Expects 'applications/json'"));
+
+        try {
+
+            let { email, password } = req.body
+
+            const salt = await bycript.genSalt(10);
+            password = await bycript.hash(password, salt);
+
+            const newUser = new User({ email, password });
+
+            newUser.save();
+
+            res.send(201);
+            next();
+
+        } catch (err) {
+
+            return next(new errors.InternalError(err.message));
+
+        }
+
+    },
+
+    auth: async (req, res, next) => {
+
+        if (!req.is('application/json')) return next(new errors.InvalidHeaderError("Expects 'application/json'"));
+
+        const { email, password } = req.body;
+
+        try {
+            // Authenticate User
+            const user = await auth.authenticate(email, password);
+
+            // Create a token
+            const token = jwt.sign(user.toJSON(), config.JWT_SECRET, {
+                expiresIn: '15m'
+            })
+
+            const { iat, exp } = jwt.decode(token);
+
+            //Response with token
+            res.send({ iat, exp, token });
+
+
+            next();
+
+        } catch (err) {
+            // User unauthorized
+            return next(new errors.UnauthorizedError(err));
+        }
+
+    }
+
+}
--- routes/user.js
+++ routes/user.js
@@ -1,88 +1,16 @@
 "use strict"
 
-const errors = require('restify-errors')
-    , bycript = require('bcryptjs')
-    , jwt = require('jsonwebtoken')
-    , User = require('../models/User')
-    , auth = require('../auth')
-    , config = require('../config');
+const controllers = require('../controllers')
+
 
 module.exports = server => {
 
     //Get Users
-    server.get('/users', async (req, res, next) => {
-
-        const users = await User.find();
-
-        try {
-
-            res.send(users);
-            res.send(200)
-            next();
-
-        } catch (err) {
-
-            return next(new errors.InternalError(err.message));
-
-        }
-    })
+    server.get('/users', controllers.User.get)
 
     //Post User
-    server.post('/register', async (req, res, next) => {
-
-        // Check for json
-        if (!req.is('application/json')) return next(new errors.InvalidHeaderError("Expects 'applications/json'"));
-
-        try {
-
-            let { email, password } = req.body
-
-            const salt = await bycript.genSalt(10);
-            password = await bycript.hash(password, salt);
-
-            const newUser = new User({ email, password });
-
-            newUser.save();
-
-            res.send(201);
-            next();
-
-        } catch (err) {
-
-            return next(new errors.InternalError(err.message));
-
-        }
-
-    });
+    server.post('/register', controllers.User.addNew);
 
     // Auth User
-    server.post('/auth', async (req, res, next) => {
-
-        if (!req.is('application/json')) return next(new errors.InvalidHeaderError("Expects 'application/json'"));
-
-        const { email, password } = req.body;
-
-        try {
-            // Authenticate User
-            const user = await auth.authenticate(email, password);
-
-            // Create a token
-            const token = jwt.sign(user.toJSON(), config.JWT_SECRET, {
-                expiresIn: '15m'
-            })
-
-            const { iat, exp } = jwt.decode(token);
-
-            //Response with token
-            res.send({ iat, exp, token });
-
-
-            next();
-
-        } catch (err) {
-            // User unauthorized
-            return next(new errors.UnauthorizedError(err));
-        }
-
-    });
+    server.post('/auth', controllers.User.auth);
 }
